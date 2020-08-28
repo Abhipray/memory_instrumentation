e55aeaeb6679a5b61a201dec94685682a265afec
Abhipray Sahoo <abhiprays@gmail.com>
2020-08-28 16:07:48 -0700

    working in linux

--- instrumentor.cpp
+++ instrumentor.cpp
@@ -1,79 +1,83 @@
 // https://blog.kummerlaender.eu/article/notes_on_function_interposition_in_cpp/
-
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
+
 #include <dlfcn.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <vector>
+#include <time.h>
 
 extern "C" {
-void *malloc(size_t size);
-int rand(void);
+#include "instrumentor.h"
 }
 
 #include <cstring>
 #include <iostream>
+#include <map>
 
-// namespace {
-// orig_malloc_f_type orig_malloc;
-// orig_free_f_type orig_free;
-// orig_calloc_f_type orig_calloc;
-// orig_realloc_f_type orig_realloc;
-// }  // namespace
+using namespace std;
+
+class Instrumentor {
+  public:
+  
+  Instrumentor(){
+    
+  }
+  void malloc(void* ptr, size_t size){
+    overall_allocations += 1;
+    time_t t = time(nullptr);
+    allocs.insert(pair<void*, pair<size_t, time_t>>(ptr, {size, t}));
+  }
+
+private:
+  map<void*, pair<size_t, time_t>> allocs;
+  uint32_t overall_allocations;
+
+};
+
+namespace {
+orig_malloc_f_type orig_malloc_;
+orig_free_f_type orig_free_;
+orig_calloc_f_type orig_calloc_;
+orig_realloc_f_type orig_realloc_;
+Instrumentor instrumentor;
+}  // namespace
+
 
 static bool is_initialized_ = false;
 
-int rand(void) {
-  typedef int (*orig_rand_f_type)();
-  orig_rand_f_type orig_rand;
-  orig_rand = (orig_rand_f_type)dlsym(RTLD_NEXT, "rand");
-  // std::cout << ("WLAA");
-  // return 43;
-  return orig_rand();  // the most random number in the universe
+
+static void initialize(void) {
+  // TODO: Convert to define
+  if (!is_initialized_) {
+    orig_malloc_ =
+    (orig_malloc_f_type)dlsym(RTLD_NEXT, "malloc");
+    orig_free_ = (orig_free_f_type)dlsym(RTLD_NEXT, "free");
+    orig_calloc_ = (orig_calloc_f_type)dlsym(RTLD_NEXT, "calloc");
+    orig_realloc_ = (orig_realloc_f_type)dlsym(RTLD_NEXT, "realloc");
+    instrumentor = Instrumentor();
+    is_initialized_ = true;
+  }
 }
 
-// static void initialize(void) {
-//   // TODO: Convert to define
-//   if (!is_initialized_) {
-//     // const void* symbol_address{dlsym(RTLD_NEXT, "malloc")};
-
-//     // std::memcpy(&actual_function, &symbol_address,
-//     sizeof(symbol_address)); orig_malloc =
-//     (orig_malloc_f_type)dlsym(RTLD_NEXT, "malloc");
-//     // orig_free = (orig_free_f_type)dlsym(RTLD_NEXT, "free");
-//     // orig_calloc = (orig_calloc_f_type)dlsym(RTLD_NEXT, "calloc");
-//     // orig_realloc = (orig_realloc_f_type)dlsym(RTLD_NEXT, "realloc");
-//     is_initialized_ = true;
-//   }
-// }
-
-// void* malloc(size_t size) {
-//   // printf("malloc intercepted");
-//   // initialize();
-//   typedef void* (*orig_malloc_f_type)(size_t size);
-//   // (void*)(orig_malloc*)(size_t){};
-//   orig_malloc_f_type orig_malloc;
-//   orig_malloc = (orig_malloc_f_type)dlsym(RTLD_NEXT, "malloc");
-
-//   // const void* symbol_address = dlsym(RTLD_NEXT, "malloc");
-//   // std::memcpy(&orig_malloc, &symbol_address, sizeof(symbol_address));
-//   return orig_malloc(size);
-// }
 
 void *malloc(size_t size) {
-  static void *(*mallocp)(size_t size);
-  char *error;
-  void *ptr;
-  typedef void *(*orig_malloc_f_type)(size_t size);
+  initialize();
+  fprintf(stderr, "Malloc %zu\n", size);
+  void* ptr = orig_malloc_(size);
+  
+  // Flag to track invocation by instrumentor
+  static bool app_invocation = true;
 
-  /* get address of libc malloc */
-  if (!mallocp) {
-    mallocp = (orig_malloc_f_type)dlsym(RTLD_NEXT, "malloc");
-    if ((error = dlerror()) != NULL) {
-      fputs(error, stderr);
-      exit(1);
-    }
+  if (app_invocation && ptr != nullptr) {
+    app_invocation = false;
+    // Any subsequent malloc calls will be made by the instrumentor
+    instrumentor.malloc(ptr, size);
+    app_invocation = true;
   }
-  ptr = mallocp(size);
-  printf("malloc(%d) = %p\n", size, ptr);
+  
+
   return ptr;
 }
